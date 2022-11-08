14c70a97ecd0fbb712877b28a03fe0269c7c299d
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-08 15:27:11 -0600

    limit exercise added extra wildcard example

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -7,3 +7,7 @@ SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 
 SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
+
+
+#underscore wildcard includes any number of characters followed by an e and then % which is any character after that
+SELECT * FROM employees WHERE last_name LIKE '___e_';
