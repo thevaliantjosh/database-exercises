d533b0a0e82b89b46cdc56ae86a82efee1f589d9
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-08 11:01:03 -0600

    clauses lecture

---
+++ clauses-lecture.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date = '1985-01-01';
+
+SELECT first_name, last_name FROM employees WHERE first_name LIKE '%sus%';
+
+SELECT DISTINCT first_name FROM employees WHERE first_name LIKE '%sus%';
+
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10026 AND 10082
+
+SELECT emp_no, first_name, last_name FROM employees WHERE last_name IN ('Herber', 'Dredge', 'Lipner', 'Baek');
+
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no <= 10026;
+
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no < 20000 AND (
+    last_name IN ('Herber', 'Baek')
+    OR first_name = 'Shridhar'
+    );
+
+SELECT first_name, last_name, gender FROM employees WHERE last_name IN ('Bolotov', 'McClurg') AND gender = 'M' ORDER BY last_name;
+
+SELECT first_name, last_name, gender FROM employees WHERE (last_name = 'Bolotov' OR last_name = 'Baek') AND gender = 'M' ORDER BY last_name;
+
+SELECT * FROM salaries WHERE salary NOT BETWEEN 50000 AND 100000;
+
+SELECT first_name, last_name, birth_date FROM employees WHERE birth_date LIKE '%-07-28'
+
+SELECT first_name, last_name, birth_date, hire_date FROM employees WHERE birth_date NOT LIKE '195%' AND hire_date NOT LIKE '199%';
+
