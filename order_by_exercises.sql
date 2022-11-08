2deeb99b99ac40882bae335029f5a7307112f127
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-11-08 12:21:06 -0600

    Order by exercise completed

---
+++ order_by_exercises.sql
@@ -0,0 +1,13 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name, emp_no DESC;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name;
+
+SELECT * FROM employees WHERE first_name IN('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no;
+
+SELECT * FROM employees WHERE last_name LIKE '%E%' ORDER BY emp_no DESC;
